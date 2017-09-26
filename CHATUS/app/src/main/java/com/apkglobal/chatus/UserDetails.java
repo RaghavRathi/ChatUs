4f5ccfc0cc2e7fb389607261068ab3d333a3fe25
Raghav Rathi <raghavrathi130796@gmail.com>
2017-09-25 17:49:56 -0700

    Chatting Application
    Chat with your friends

---
+++ CHATUS/app/src/main/java/com/apkglobal/chatus/UserDetails.java
@@ -0,0 +1,14 @@
+package com.apkglobal.chatus;
+
+/**
+ * Created by dell on 7/28/2017.
+ */
+public class UserDetails {
+    /*user  data from the server
+    *
+    * */
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
+
